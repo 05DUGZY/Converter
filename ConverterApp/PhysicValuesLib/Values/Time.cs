3948cacd915ff673b3171e1485b4d50e33e9f4db
05DUGZY <158441679+05DUGZY@users.noreply.github.com>
2024-03-05 08:51:27 +0300

    Add files via upload

---
+++ ConverterApp/PhysicValuesLib/Values/Time.cs
@@ -0,0 +1,107 @@
+﻿namespace PhysicValuesLib.Values;
+
+internal class Time : AnyValue
+{
+    public Time()
+    {
+        ValueName = "Время";
+
+        CoefficientsAndMeasuresList = new Dictionary<string, double>()
+        {
+            { "Секунды",        1       },
+            { "Милисекунды",    0.001   },
+            { "Минуты",         60      },
+            { "Часы",           60 * 60 },
+        };
+    }
+}
+internal class distanse : AnyValue
+{
+    public distanse()
+    {
+        ValueName = "Расстояние";
+
+        CoefficientsAndMeasuresList = new Dictionary<string, double>()
+        {
+            { "метр",        1       },
+            { "Милиметр",    0.001   },
+            { "сантиметр",   0.1     },
+            { "километр",    1000    },
+        };
+    }
+}
+internal class weight : AnyValue
+{
+    public weight()
+    {
+        ValueName = "Масса";
+
+        CoefficientsAndMeasuresList = new Dictionary<string, double>()
+        {
+            { "килограмм", 1      },
+            { "грамм",     0.001  },
+            { "центнер",   100    },
+            { "тонна",     1000   },
+        };
+    }
+}
+internal class square : AnyValue
+{
+    public square()
+    {
+        ValueName = "Площадь";
+
+        CoefficientsAndMeasuresList = new Dictionary<string, double>()
+        {
+            { "метр²",        1       },
+            { "Милиметр²",    0.00001 },
+            { "сантиметр²",   0.01    },
+            { "километр²",    1000000 },
+        };
+    }
+}
+internal class speed : AnyValue
+{
+    public speed()
+    {
+        ValueName = "Скорость";
+
+        CoefficientsAndMeasuresList = new Dictionary<string, double>()
+        {
+            { "метр в секунду",  1       },
+            { "Миля в час",      2.237   },
+            { "узел",            1.94384 },
+            { "километр в час",  3.6     },
+        };
+    }
+}
+internal class ee : AnyValue
+{
+    public ee()
+    {
+        ValueName = "Давление";
+
+        CoefficientsAndMeasuresList = new Dictionary<string, double>()
+        {
+            { "паскаль",    1          },
+            { "бар",        100000     },
+            { "атмосфера",  9.86923267 },
+            { "торр",       0.00750062 },
+        };
+    }
+}
+internal class  r: AnyValue
+{
+    public r()
+    {
+        ValueName = "Объём данных";
+
+        CoefficientsAndMeasuresList = new Dictionary<string, double>()
+        {
+            { "байт",        1         },
+            { "бит",         8         },
+            { "килобайт",    0.008     },
+            { "мегабайт",    0.0000001 },
+        };
+    }
+}
---
+++ ConverterApp/WindowsAPI/ConverterForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsAPI
+{
+    partial class ConverterForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 35);
+            listBox1.Margin = new Padding(3, 2, 3, 2);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(236, 154);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(271, 35);
+            listBox2.Margin = new Padding(3, 2, 3, 2);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(236, 154);
+            listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 15;
+            listBox3.Location = new Point(532, 35);
+            listBox3.Margin = new Padding(3, 2, 3, 2);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(236, 154);
+            listBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(11, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(128, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Физическая величина";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(271, 10);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Откуда переводим";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(532, 10);
+            label3.Name = "label3";
+            label3.Size = new Size(96, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Куда переводим";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(796, 35);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(196, 23);
+            textBox1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(796, 10);
+            label4.Name = "label4";
+            label4.Size = new Size(174, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Ввести значение для перевода";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(796, 132);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(195, 56);
+            button1.TabIndex = 8;
+            button1.Text = "Перевести";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += BtnConvert_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(798, 94);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(196, 23);
+            textBox2.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(794, 69);
+            label5.Name = "label5";
+            label5.Size = new Size(69, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Выполнить";
+            // 
+            // ConverterForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1002, 206);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "ConverterForm";
+            Text = "Конвертер";
+            Load += ConverterFormLoad;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label4;
+        private Button button1;
+        private TextBox textBox2;
+        private Label label5;
+    }
+}
